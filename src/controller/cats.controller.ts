927ac7b76d7842058440e95d960e41c3281dc729
QinZhen001 <qz519189636@qq.com>
2023-10-15 20:22:18 +0800

    feat: demo

---
+++ src/controller/cats.controller.ts
@@ -0,0 +1,12 @@
+// https://nest.nodejs.cn/controllers
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
+
+@Controller('cats')
+export class CatsController {
+  @Get()
+  findAll(@Req() request: Request): string {
+    console.log(request);
+    return 'This action returns all cats';
+  }
+}
